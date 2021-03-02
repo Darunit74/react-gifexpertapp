e96946e6f102a9bcff156cd94415cde7057490ae
Andrés Carreño <brayanandre74@hotmail.com>
2021-03-02 00:18:47 -0500

    First Commit

---
+++ src/helpers/GetGifs.js
@@ -0,0 +1,21 @@
+
+  export const getGifs = async ( category ) => {
+        
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=vzNqKaTB5o7sfU0phmOWqBBkEaeIDXdM`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+
+        const { id, title, images } = img;
+
+        return {
+            id: id,
+            title: title,
+            url: images?.downsized_medium.url
+        }
+    });
+
+     return gifs;
+
+}
